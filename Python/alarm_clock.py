48c599078d49f2000b474f50b72ca61ce02befe9
Jared Cooper <152523733+autoimmunexd@users.noreply.github.com>
2024-01-11 20:51:06 -0500

    Update alarm_clock.py
    Working on outputting the time to a label on the gui.

--- Python/alarm_clock.py
+++ Python/alarm_clock.py
@@ -9,22 +9,39 @@ class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Auto's Alarm")
-        self.setFixedWidth(600)
-        self.setFixedHeight(400)
+        self.setGeometry(100, 100, 300, 150)
         button = QPushButton("Eastern Time")
         button.setFixedSize(QSize(100, 100))
         self.setCentralWidget(button)
 
 class AlarmClock:
-    def __init__(self, time):
-        _state = 'OFF'
-        self.time = time
+    def __init__(self):
+        self.state = 'OFF'
+        self.time = datetime.now()
 
-    def getCurrentTime(self, time):
-        print(time)
+    def getCurrentTime(self):
+        #converts the datetime.now() to a 12 hour readable format.
+        print(self.time.strftime("%I:%M:%S%p"))
+
+    def getState(self):
+        if self.state == 'OFF':
+            print('The alarm is off.')
+        else:
+            print('The alarm is on.')
 
 
-        
+    #working on this part right here, printing the current time to a label
+    #     self.timer = QTimer(self)
+    #     self.timer.timeout.connect(self.update_time)
+    #     self.timer.start(1000)  # 1000 milliseconds = 1 second
+    #     # Initial update
+    #     self.update_time()
+
+    # def update_time(self):
+    #     current_time = datetime.now().strftime("%Y-%m-%d %I:%M:%S %p")
+    #     self.time_label.setText(current_time)
+
+
 #settings management with json
 #         import json
 
@@ -37,13 +54,14 @@ class AlarmClock:
 # with open('config.json', 'r') as jsonfile:
 #     loaded_settings = json.load(jsonfile)
 
-
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     window = MainWindow()
     window.show()
+    clock = AlarmClock()
+    clock.getState()
+    clock.getCurrentTime()
     sys.exit(app.exec_())
-
 
 #click button to set the current timezone
 #current time
