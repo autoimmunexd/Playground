3ea805083f7a5d997311436950e1b6c1f8dae8be
Jared Cooper <152523733+autoimmunexd@users.noreply.github.com>
2024-01-31 10:59:02 -0500

    updated alarm clock

--- Python/alarm_clock/alarm_clock.py
+++ Python/alarm_clock/alarm_clock.py
@@ -1,13 +1,69 @@
+import time
+import os.path
+import configparser
+import threading
+import eel
+
+config = configparser.ConfigParser()
+
 class AlarmClock:
-    def __init__(self, current_time, am_pm, alarm_state):
-        self.current_time = current_time
-        self.am_pm = am_pm
-        self.alarm_state = alarm_state
+    def __init__(self):
+        #binds the thread to the specific instance of the class.  targets the tick() method and runs it in a seperate thread to stop blocking
+        self.tick_thread = threading.Thread(target=self.tick)
+        #if the config file exists
+        if os.path.isfile('config.ini'):
+            #open the config file and set to variable configfile
+            with open('config.ini', 'r') as configfile:
+                #read() the config file
+                configfile.read()
+        else:
+            #if the config file doesn't exist, make one that looks like this with default values
+            config['Clock'] = {
+    'alarm_state': '',
+    'alarm_time': '',
+    'alarm_sound': ''
+}           #create a config.ini file
+            with open('config.ini', 'w') as configfile:
+                #write the config ini file with the config default values
+                config.write(configfile)
+        #clock is running init method so set the current time to the current time
+        self.current_time = time.strftime("%I:%M %p")
+        #set am_pm
+        self.am_pm = None
+        #set alarm_state
+        self.alarm_state = None
+        #decides if it's morning or night based off the array index of 1 after splitting at the space in the returned string from current_time
+        if self.current_time.split(' ')[1] == 'AM':
+            self.am_pm = 'AM'
+        else:
+            self.am_pm = 'PM'
 
-    def update_time():
-        pass
+        #decorate to add functionality to the eel class with our update time method
+    @eel.expose
+        #update time method calls the javascript function and passes in a value of another
+        #function that returns the current time of the instance of the alarmclock class
+    def update_time_ui(self, time):
+        eel.updateTime(time)
 
-    def update_am_pm():
-        pass
+    def update_time(self):
+        self.current_time = time.strftime("%I:%M %p")
+        self.update_time_ui(self.current_time)
+        #print(self.current_time)
+        
+    
+    def tick(self):
+        while True:
+            self.update_time()
+            time.sleep(1)
+            print('tik')
 
-myclock = AlarmClock()
+# if __name__ == '__main__':
+#     print(' hello world!')
+#     myclock = AlarmClock()
+
+
+
+
+
+#value = config.get('section_name', 'key_name')
+# print(value)
