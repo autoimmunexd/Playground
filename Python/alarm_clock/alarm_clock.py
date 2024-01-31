bee2f42386a70ab9a7e7bf68197edf04d5a08b5a
Jared Cooper <152523733+autoimmunexd@users.noreply.github.com>
2024-01-30 23:27:55 -0500

    Update alarm_clock.py

---
+++ Python/alarm_clock/alarm_clock.py
@@ -0,0 +1,13 @@
+class AlarmClock:
+    def __init__(self, current_time, am_pm, alarm_state):
+        self.current_time = current_time
+        self.am_pm = am_pm
+        self.alarm_state = alarm_state
+
+    def update_time():
+        pass
+
+    def update_am_pm():
+        pass
+
+myclock = AlarmClock()
